3054f9247e026a4bd65bdac58b62cfdf76849886
Artem Morozov <bars834@gmail.com>
2023-03-05 16:34:09 +0300

    first commit

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 // form submission
 
-// $(document).ready(function () {
+$(document).ready(function () {
 
-// 	$('form').submit(function (event) {
+	$('form').submit(function (event) {
 
-// 		event.preventDefault();
+		event.preventDefault();
 
-// 		$.ajax({
-// 			type: $(this).attr('method'),
-// 			url: $(this).attr('action'),
-// 			data: new FormData(this),
-// 			contentType: false,
-// 			cache: false,
-// 			processData: false,
-// 			success: function (result) {
-// 				alert('Sent!')
-// 			}
-// 		});
-// 	});
-// });
+		$.ajax({
+			type: $(this).attr('method'),
+			url: $(this).attr('action'),
+			data: new FormData(this),
+			contentType: false,
+			cache: false,
+			processData: false,
+			success: function (result) {
+				alert('Sent!')
+			}
+		});
+	});
+});
 
 
 // swiper
